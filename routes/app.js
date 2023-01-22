71d44f205845d93774b4696cba65e7345ea77d93
swarup-tester <swarup.tester@gmail.com>
2023-01-22 11:20:28 +0530

    File has been uploaded
    File Uploaded

---
+++ routes/app.js
@@ -0,0 +1,81 @@
+const multer = require("multer");
+const path = require("path");
+const express = require('express');
+const router = express.Router();
+
+// Access the Model file
+const Post = require('../model/post');
+
+// const MIME_TYPE_MAP = {
+//     "image/png": "png",
+//     "image/jpeg": "jpg",
+//     "image/jpg": "jpg"
+// };
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         const isValid = MIME_TYPE_MAP[file.mimetype];
+//         let error = new Error("Invalid mime type");
+//         if (isValid) {
+//             error = null;
+//         }
+//         cb(error, "./images");
+//     },
+//     filename: (req, file, cb) => {
+//         const name = file.originalname
+//             .toLowerCase()
+//             .split(" ")
+//             .join("-");
+//         const ext = MIME_TYPE_MAP[file.mimetype];
+//         cb(null, name + "-" + Date.now() + "." + ext);
+//     }
+// });
+
+// Add Post
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './images/');
+    },
+    filename: (req, file, cb) => {
+        const name = file.originalname
+            .toLowerCase()
+            .split(" ")
+            .join("-");
+
+        cb(null, Date.now() + "-" + name);
+    }
+});
+
+// localhost:3000/api/posts
+// These are the keys {title:content:image}
+router.post("/api/posts", multer({ storage: storage }).single("image"),
+    (req, res, next) => {
+        const url = req.protocol + "://" + req.get("host");
+        const post = new Post({
+            title: req.body.title,
+            content: req.body.content
+            // imagePath: url + "/images/" + req.file.filename
+        });
+
+        post.save().then(createPost => {
+            res.status(201).json({
+                message: 'Post added successfully',
+                post: {
+                    id: createPost._id,
+                    title: createPost.title,
+                    content: createPost.content,
+                    // imagePath: createPost.imagePath
+                }
+            });
+        });
+    })
+
+router.post('/numbers', (req, res) => {
+    const N1 = Number(req.body.n1);
+    const N2 = Number(req.body.n2);
+    if (N1 > N2) res.json({ result: N1 });
+    else res.json({ result: N2 });
+})
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+// Database
+//const db = "mongodb+srv://newtechswarup:ZjGsAWigX8fG0jRv@cluster0.g6lll.mongodb.net/payroll?retryWrites=true&w=majority";
+const db = "mongodb+srv://swarupnode:ju6xysfOTUTwvRVy@cluster0.tfzh6.mongodb.net/account_db?retryWrites=true&w=majority";
+
+//const db = "mongodb+srv://techswarup:qwerty@1@cluster0.g6lll.mongodb.net/payroll?retryWrites=true&w=majority";
+
+mongoose.connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    function(err) {
+        if (err) {
+            console.error(err)
+        } else {
+            console.log('Connect Live MongoDB')
+        }
+    });
+
+const postRoutes = require('./routes/app');
+//This middleware will allow file to be read/GET when performed select Query
+// app.use("/images", express.static(path.join("images")));
+
+app.use(express.json());
+app.use(postRoutes);
+
+app.get('/',(req,res)=>{
+    res.send('Api Connected');
+})
+
+
+app.listen(port);
